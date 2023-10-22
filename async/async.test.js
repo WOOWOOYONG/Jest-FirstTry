96c99d74e2f489b497c2725a2f8beb58e4569015
woowooyong <woowooyong@gmail.com>
2023-10-22 22:58:24 +0800

    update: 新增Mocks練習

--- async/async.test.js
+++ async/async.test.js
@@ -1,6 +1,19 @@
 const fetchData = require('./async');
+const axios = require('axios')
 
 test('it should return a todo', async () => {
   const todo = await fetchData(1);
   expect(todo.id).toBe(1);
+});
+
+test('mock axios', async () => {
+  const spy = jest.spyOn(axios, 'get').mockReturnValueOnce({
+    data: {
+      id: 1,
+      todo: 'Do Homework!',
+    },
+  });
+  const res = await fetchData(1);
+  expect(res.todo).toBe('Do Homework!');
+  spy.mockRestore();
 });
